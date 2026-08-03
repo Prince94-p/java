80bad61a9d32df431d59d4bb23bef4af158364e1
Prince94-p <prince9470p@gmail.com>
2026-08-03 23:11:12 +0530

    PRACT1-1

---
+++ p11.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+enum Coin {
+    ONE, TWO, FIVE, TEN
+}
+
+public class p11 {
+    public static void main(String[] args) {
+        int snackPrice = 15;
+        int total = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        while (total < snackPrice) {
+            System.out.print("Enter coin (ONE, TWO, FIVE, TEN): ");
+            Coin coin = Coin.valueOf(sc.next().toUpperCase());
+
+            int value = switch (coin) {
+                case ONE -> 1;
+                case TWO -> 2;
+                case FIVE -> 5;
+                case TEN -> 10;
+            };
+
+            total += value;
+            System.out.println("Total so far: " + total);
+        }
+
+        System.out.println("Paid. Change: " + (total - snackPrice));
+
+        sc.close();
+    }
+}
